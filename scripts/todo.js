e3a10c2b5dd41d36483286d652505f2ddecfa068
tomohiro <tomohiroakazawa@gmail.com>
2020-04-04 21:01:23 +0900

    modified

--- scripts/todo.js
+++ scripts/todo.js
@@ -25,9 +25,13 @@ module.exports = (robot) => {
 		msg.send('削除しました: ' + task);
 	});
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
+		const list = todo.list();
+		if(!list) msg.send('(TODOはありません)');
+		else msg.send(todo.list().join('\n'));
 	});
 	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
+		const donelist = todo.donelist();
+		if(!donelist) msg.send('(完了したTODOはありません)');
+		else msg.send(todo.donelist().join('\n'));
 	});
 };
